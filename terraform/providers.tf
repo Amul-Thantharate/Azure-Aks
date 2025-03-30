5bd4294c42b5a3baba23a659b765d23aac38985c
Amul-Thantharate <amulthantharate@gmail.com>
2025-03-30 14:58:55 +0530

    Update .gitignore and remove sensitive information from providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
   features {}
-  subscription_id = "62beb424-25dc-4b94-aee1-5742c4dbf8ad"
-  tenant_id       = "77ef7cda-33ba-4bbe-b863-6070d8c3864c"
+  subscription_id = ""
+  tenant_id       = ""
 }
